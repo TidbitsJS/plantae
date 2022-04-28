c5df2949d9d70b83db669675dfaac56efdc7b2ba
Tidbits JS <tidbitsjs@gmail.com>
2022-04-28 22:36:53 +0530

    routes config

--- app.ts
+++ app.ts
@@ -1,35 +1,48 @@
-import express from 'express';
-import * as http from 'http';
-import * as winston from 'winston';
-import * as expressWinston from 'express-winston';
-import debug from 'debug';
+import express from "express";
+import * as http from "http";
+import * as winston from "winston";
+import * as expressWinston from "express-winston";
+import debug from "debug";
+import cors from "cors";
+
+import { CommonRoutesConfig } from "./common/common.routes.config";
+import { PlantsRoutes } from "./plants/plants.routes.config";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 4040
+const port = 4040;
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: debug.IDebugger = debug("app");
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const loggerOptions: expressWinston.LoggerOptions = {
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-        winston.format.json(),
-        winston.format.prettyPrint(),
-        winston.format.colorize({all: true})
-    )
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true })
+  ),
+};
+
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false;
 }
 
-if(!process.env.DEBUG) {
-    loggerOptions.meta = false;
-}
+app.use(expressWinston.logger(loggerOptions));
 
-app.use(expressWinston.logger(loggerOptions))
+routes.push(new PlantsRoutes(app));
 
 const runningMessage = `Server running at http://localhost:${port}`;
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200).send("SERVER " + runningMessage);
-})
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).send("SERVER " + runningMessage);
+});
 
 server.listen(port, () => {
-    console.log(runningMessage);
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
+
+  console.log(runningMessage);
 });
