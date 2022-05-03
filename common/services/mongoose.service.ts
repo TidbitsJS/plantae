b054f35a377e5137de0d48cc3970e7d4466c93f8
Tidbits JS <tidbitsjs@gmail.com>
2022-05-03 23:49:18 +0530

    API testing with mongoose connection

--- common/services/mongoose.service.ts
+++ common/services/mongoose.service.ts
@@ -6,14 +6,9 @@ const log: debug.IDebugger = debug("app:mongoose-service");
 class MongooseService {
   private count = 0;
   private mongooseOptions = {
-    // without this set to true, Mongoose prints a deprecation warning
     useNewUrlParser: true,
-    // Mongoose documentation recommends setting thi to use a newer connection management engine
     useUnifiedTopology: true,
-    // to provide helpful feedback
     serverSelectionTimeoutMS: 5000,
-    // Causes Mongoose to use a newer native MongoDB feature instead of an older Mongoose shim
-    useFindAndModify: false,
   };
 
   constructor() {
@@ -24,23 +19,22 @@ class MongooseService {
     return mongoose;
   }
 
-  connectWithRetry() {
-    log(`Attempting MongoDB connection (will retry if needed)`);
+  connectWithRetry = () => {
+    log("Attempting MongoDB connection (will retry if needed)");
     mongoose
-      .connect(process.env.MONGO_URI as string, this.mongooseOptions)
+      .connect(process.env.MONGO_URL as string, this.mongooseOptions)
       .then(() => {
-        log("MongoDB connection successful");
+        log("MongoDB is connected");
       })
       .catch((err) => {
-        const retrySecons = 5;
+        const retrySeconds = 5;
         log(
-          `MongoDb connection unsuccessful (will retry #${++this
-            .count} after ${retrySecons} seconds): ${err.message}`
+          `MongoDB connection unsuccessful (will retry #${++this
+            .count} after ${retrySeconds} seconds):`,
+          err
         );
-
-        setTimeout(this.connectWithRetry, retrySecons * 1000);
+        setTimeout(this.connectWithRetry, retrySeconds * 1000);
       });
-  }
+  };
 }
-
 export default new MongooseService();
