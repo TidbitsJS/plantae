12261fb07b3d0ada9d1ddc4d0147c7489be43221
Tidbits JS <tidbitsjs@gmail.com>
2022-05-10 23:14:45 +0530

    user hashed password

--- users/controllers/users.controller.ts
+++ users/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import debug from "debug";
+import argon2 from "argon2";
 
 import usersService from "../services/users.service";
 
@@ -17,17 +18,22 @@ class UsersController {
   }
 
   async createUser(req: express.Request, res: express.Response) {
+    req.body.password = await argon2.hash(req.body.password);
     const userId = await usersService.create(req.body);
     log("User created", userId);
     res.status(201).send({ id: userId });
   }
 
   async patch(req: express.Request, res: express.Response) {
+    if (req.body.password) {
+      req.body.password = await argon2.hash(req.body.password);
+    }
     log(await usersService.patchById(req.body.id, req.body));
     res.status(204).send();
   }
 
   async put(req: express.Request, res: express.Response) {
+    req.body.password = await argon2.hash(req.body.password);
     log(await usersService.putById(req.body.id, req.body));
     res.status(204).send();
   }
